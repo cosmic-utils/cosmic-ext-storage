5ba329652f8d3d85ed7f13b322bb223bce789f94
stoorps <stoorps@pm.me>
2026-07-23 22:28:29 +0100

    chore: keep workspace clippy clean

--- src/state/dialogs.rs
+++ src/state/dialogs.rs
@@ -6,6 +6,7 @@ use storage_types::{
 };
 
 #[derive(Debug, Clone)]
+#[allow(clippy::large_enum_variant)]
 pub enum ShowDialog {
     DeletePartition(DeletePartitionDialog),
     AddPartition(CreatePartitionDialog),
